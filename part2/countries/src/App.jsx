c008fe10e902cd827113536ed8c935ae06626e70
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-05-15 17:22:47 +0200

    Redid exercise 2.18 with clearer syntax

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,10 +2,59 @@ import { useState, useEffect } from "react";
 import countryService from "./services/countries";
 import "./index.css";
 
+const Filter = (props) => {
+	return (
+		<label>
+			find countries
+			<input value={props.value} onChange={props.onChange} />
+		</label>
+	);
+};
+
+const CountriesList = ({ filteredCountries }) => {
+	if (filteredCountries.length > 10) {
+		return <div>Too many countries to show</div>;
+	}
+	if (filteredCountries.length <= 10 && filteredCountries.length > 1) {
+		return (
+			<div>
+				{filteredCountries.map((country) => (
+					<p key={country.cca3}>
+						{country.name.common} <button type="button">Show</button>
+					</p>
+				))}
+			</div>
+		);
+	}
+	if (filteredCountries.length === 1) {
+		const country = filteredCountries[0];
+		const capital = country.capital[0];
+		const cca3 = country.cca3;
+		const name = country.name.common;
+		const area = country.area;
+		const languages = country.languages;
+		const flag = country.flags.png;
+		return (
+			<div key={cca3}>
+				<h1>{name}</h1>
+				<p>Capital {capital}</p>
+				<p>Area {area}</p>
+				<h2>Languages</h2>
+				<ul>
+					{Object.values(languages).map((value) => (
+						<li key={value}>{value}</li>
+					))}
+				</ul>
+				<img className="flag" src={flag} alt={`Flag of ${name}`} />
+			</div>
+		);
+	}
+};
+
 const App = () => {
 	const [searchInput, setSearchInput] = useState("");
 	const [countries, setCountries] = useState([]);
-	const [country, setCountry] = useState("");
+	const [renderSingleCountry, setRenderSingleCountry] = useState(false);
 
 	useEffect(() => {
 		countryService.getAll().then((countries) => {
@@ -18,87 +67,19 @@ const App = () => {
 		console.log(event.target.value);
 	};
 
-	const countriesToShow =
+	const filteredCountries =
 		searchInput !== ""
 			? countries.filter((c) =>
 					c.name.common.toLowerCase().includes(searchInput.toLowerCase()),
 				)
 			: countries;
 
-	const SingleCountryData = ({ country }) => {
-		console.log(country.languages);
-		console.log(Object.values(country.languages));
-		const cca3 = country.cca3;
-		const name = country.name.common;
-		const capital = country.capital[0];
-		const area = country.area;
-		const languages = country.languages;
-		const flag = country.flags.png;
-		return (
-			<div>
-				<div key={cca3}>
-					<h1>{name}</h1>
-					<p>Capital {capital}</p>
-					<p>Area {area}</p>
-					<h2>Languages</h2>
-					<ul>
-						{Object.values(languages).map((value) => (
-							<li key={value}>{value}</li>
-						))}
-					</ul>
-					<img className="flag" src={flag} alt={`Flag of ${name}`} />
-				</div>
-			</div>
-		);
-	};
-
-	const Country = () => {
-		const showCountry = (country) => {
-			return <SingleCountryData country={country} />;
-		};
-
-		if (searchInput === "") {
-			return <div>No search input yet</div>;
-		}
-
-		if (countriesToShow.length > 10) {
-			return <div>Too many countries to show</div>;
-		}
-		if (countriesToShow.length <= 10 && countriesToShow.length > 1) {
-			return (
-				<div>
-					{countriesToShow.map((c) => (
-						<p key={c.cca3}>
-							{c.name.common}
-							<button
-								type="button"
-								onClick={() => {
-									showCountry(c);
-								}}
-							>
-								Show
-							</button>
-						</p>
-					))}
-				</div>
-			);
-		}
-		if (countriesToShow.length === 1) {
-			return <SingleCountryData country={countriesToShow[0]} />;
-		}
-	};
-
 	return (
 		<>
 			<h2>Search</h2>
-			<div>
-				<label>
-					filter shown with
-					<input value={searchInput} onChange={handleSearchChange} />
-				</label>
-			</div>
+			<Filter value={searchInput} onChange={handleSearchChange} />
 			<h2>Countries</h2>
-			<Country />
+			<CountriesList filteredCountries={filteredCountries} />
 			<h2>Weather in [Capital]</h2>
 		</>
 	);
