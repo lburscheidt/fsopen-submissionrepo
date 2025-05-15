3f11d4fa3ebf67b421f673f6258dd292160f3cb0
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-05-15 11:54:47 +0200

    work on button for exercise 2.19

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 const App = () => {
 	const [searchInput, setSearchInput] = useState("");
 	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState("");
 
 	useEffect(() => {
 		countryService.getAll().then((countries) => {
@@ -24,7 +25,38 @@ const App = () => {
 				)
 			: countries;
 
-	const showCountry = () => {
+	const SingleCountryData = ({ country }) => {
+		console.log(country.languages);
+		console.log(Object.values(country.languages));
+		const cca3 = country.cca3;
+		const name = country.name.common;
+		const capital = country.capital[0];
+		const area = country.area;
+		const languages = country.languages;
+		const flag = country.flags.png;
+		return (
+			<div>
+				<div key={cca3}>
+					<h1>{name}</h1>
+					<p>Capital {capital}</p>
+					<p>Area {area}</p>
+					<h2>Languages</h2>
+					<ul>
+						{Object.values(languages).map((value) => (
+							<li key={value}>{value}</li>
+						))}
+					</ul>
+					<img className="flag" src={flag} alt={`Flag of ${name}`} />
+				</div>
+			</div>
+		);
+	};
+
+	const Country = () => {
+		const showCountry = (country) => {
+			return <SingleCountryData country={country} />;
+		};
+
 		if (searchInput === "") {
 			return <div>No search input yet</div>;
 		}
@@ -33,18 +65,17 @@ const App = () => {
 			return <div>Too many countries to show</div>;
 		}
 		if (countriesToShow.length <= 10 && countriesToShow.length > 1) {
-			const cca3 = countriesToShow.map((c) => c.cca3);
-			const name = countriesToShow.map((c) => c.name.common);
-			const capital = countriesToShow.map((c) => c.capital[0]);
-			const area = countriesToShow.map((c) => c.area);
-			const languages = countriesToShow.map((c) => c.languages);
-			const flag = countriesToShow.map((c) => c.flags.png);
 			return (
 				<div>
-					{countriesToShow.map((country) => (
-						<p key={country.cca3}>
-							{country.name.common}
-							<button type="button" onClick={showCountry}>
+					{countriesToShow.map((c) => (
+						<p key={c.cca3}>
+							{c.name.common}
+							<button
+								type="button"
+								onClick={() => {
+									showCountry(c);
+								}}
+							>
 								Show
 							</button>
 						</p>
@@ -53,28 +84,7 @@ const App = () => {
 			);
 		}
 		if (countriesToShow.length === 1) {
-			const cca3 = countriesToShow.map((c) => c.cca3);
-			const name = countriesToShow.map((c) => c.name.common);
-			const capital = countriesToShow.map((c) => c.capital[0]);
-			const area = countriesToShow.map((c) => c.area);
-			const languages = countriesToShow.map((c) => c.languages);
-			const flag = countriesToShow.map((c) => c.flags.png);
-			return (
-				<div>
-					<div key={cca3}>
-						<h1>{name}</h1>
-						<p>Capital {capital}</p>
-						<p>Area {area}</p>
-						<h2>Languages</h2>
-						<ul>
-							{Object.keys(languages[0]).map((key) => (
-								<li key={key}>{languages[0][key]}</li>
-							))}
-						</ul>
-						<img className="flag" src={flag} alt={`Flag of ${name}`} />
-					</div>
-				</div>
-			);
+			return <SingleCountryData country={countriesToShow[0]} />;
 		}
 	};
 
@@ -88,7 +98,7 @@ const App = () => {
 				</label>
 			</div>
 			<h2>Countries</h2>
-			{showCountry()}
+			<Country />
 			<h2>Weather in [Capital]</h2>
 		</>
 	);
