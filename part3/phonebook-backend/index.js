34411575502ce6823e7a67a91d7f9ce69eea8235
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2025-05-17 17:20:57 +0200

    Rewrote delete operators to undefined returns

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -14,8 +14,8 @@ const Person = mongoose.model("Person", personSchema);
 personSchema.set("toJSON", {
 	transform: (document, returnedObject) => {
 		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
+		returnedObject._id = undefined;
+		returnedObject.__v = undefined;
 	},
 });
 
